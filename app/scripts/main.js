90205da9e8534545e5ffc100d192f84927f4690c
Gil Desmarais <gil+git@posteo.de>
2015-01-25 18:07:06 +0100

    Initial commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,13 @@
 (function() {
   'use strict';
+
+  if (window.console && window.console.log) {
+    var font = 'font-family:"Ubuntu Mono", Monaco, monospaced; font-size:16px';
+
+    window.console.log('%cHey!', 'font-weight:bold;' + font);
+    window.console.log('%cLooks like you are interested in my work.', font);
+    window.console.log('%cI prefer writing maintainable CSS with BEM notation.', font);
+    window.console.log('%cYou can find the complete code, with grunt setup etc., of my website on github:', font);
+    window.console.log('%chttp://github.com/giLL0r/website', font);
+  }
 }());
