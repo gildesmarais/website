3ca4b957196c501aca904a0a7afc265be934f435
Gil Desmarais <gil+git@posteo.de>
2015-02-02 17:48:34 +0100

    feat(): Add a whole bunch of new stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,9 +5,9 @@
     var font = 'font-family:"Ubuntu Mono", Monaco, monospaced; font-size:16px';
 
     window.console.log('%cHey!', 'font-weight:bold;' + font);
-    window.console.log('%cLooks like you are interested in my work.', font);
-    window.console.log('%cI prefer writing maintainable CSS with BEM notation.', font);
-    window.console.log('%cYou can find the complete code, with grunt setup etc., of my website on github:', font);
+    window.console.log('%cLooks like you are interested in my work. That\'s cool!', font);
+    window.console.log('%cYou can find the git-repository of this website on github:', font);
     window.console.log('%chttp://github.com/giLL0r/website', font);
+    window.console.log('%cIf you like it and want to get to know me better, contact me!', font);
   }
 }());
