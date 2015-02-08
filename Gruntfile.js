b1afde630ddcd5201bec658a3fb1a3dbac1fb462
Gil Desmarais <gil+git@posteo.de>
2015-02-08 16:15:35 +0100

    fix(portrait): Portrait images not available in @2x resolution
    - Exclude them from filerev task

--- Gruntfile.js
+++ Gruntfile.js
@@ -232,6 +232,7 @@ module.exports = function(grunt) {
             '<%= config.dist %>/scripts/{,*/}*.js',
             '<%= config.dist %>/styles/{,*/}*.css',
             '<%= config.dist %>/images/{,*/}*.*',
+            '!<%= config.dist %>/images/gil*.jpg',
             '<%= config.dist %>/styles/fonts/{,*/}*.*',
             '<%= config.dist %>/*.{ico,png}'
           ]
