1cf0df8883ab5e6ee59165c4d6ef41583bd62fda
Gil Desmarais <gil@desmarais.de>
2017-05-08 14:09:03 +0200

    feat(experiences): Update work experiences

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,7 +2,8 @@
   'use strict'
 
   var element = document.querySelector('[data-timed-display]')
-  if ((Date.now() - Date.parse(element.dataset.postDate)) < 60 * 60 * 24 * 30 * 1000) {
+
+  if (element && (Date.now() - Date.parse(element.dataset.postDate)) < 60 * 60 * 24 * 30 * 1000) {
     element.classList.toggle('index__post--hidden')
   }
 }())
