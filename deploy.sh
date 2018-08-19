1c940254400eafe5fdb28c8e611676276c90dfc1
Gil Desmarais <gil@desmarais.de>
2018-08-19 13:08:22 +0200

    fix(deploy): invalid argument to htmlminifier

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 JEKYLL_ENV=production bundle exec jekyll build
-html-minifier --case-sensitive --collapse-boolean-attributes --collapse-whitespace --minify-ur-ls=true --minify-css=true --minify-js=true --remove-comments --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir _site --output-dir _site --file-ext html
-rsync -avz --delete -e ssh _site/ gil-drop1:/var/www/gil.desmarais.de/html
+html-minifier --case-sensitive --collapse-boolean-attributes --collapse-whitespace --minify-urls=true --minify-css=true --minify-js=true --remove-comments --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir _site --output-dir _site --file-ext html
+rsync -avz --delete -e ssh _site/ website:/var/www/gil.desmarais.de
