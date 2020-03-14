89f4e028ff6b85904c530ea0a6a662797c36aa55
Gil Desmarais <gildesmarais@users.noreply.github.com>
2020-03-14 18:12:16 +0100

    feat: setup publishing with github actions (#14)
    * feat: setup publishing with github actions
    
    * ..

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-JEKYLL_ENV=production bundle exec jekyll build
-npx html-minifier --case-sensitive --collapse-boolean-attributes --collapse-whitespace --minify-urls=true --minify-css=true --minify-js=true --remove-comments --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --use-short-doctype --input-dir _site --output-dir _site --file-ext html
-rsync -avz --delete -e ssh _site/ website:/var/www/gil.desmarais.de
