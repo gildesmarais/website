93e2b48033e8bced7ca7053e88c20404728d2d28
Gil Desmarais <gildesmarais@users.noreply.github.com>
2020-03-15 13:41:21 +0100

    feat: upgrade to jekyll 4 (#15)
    * feat: upgrade to jekyll 4
    
    remove jekyll-assets
    
    * .
    
    * .
    
    * .
    
    * away with yarn
    
    * ..
    
    * .
    
    * .
    
    * fricking bundler version
    
    * remove deps

--- _assets/javascript/main.js
+++
@@ -1,9 +0,0 @@
-;(function() {
-  "use strict"
-
-  var element = document.querySelector("[data-timed-display]")
-
-  if (element && Date.now() - Date.parse(element.dataset.postDate) < 60 * 60 * 24 * 30 * 1000) {
-    element.classList.toggle("index__post--hidden")
-  }
-})()
