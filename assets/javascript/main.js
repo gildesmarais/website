3d43979dab888097cfe2df7256ca5144e81d4ad2
Gil Desmarais <git@desmarais.de>
2020-03-29 18:53:07 +0200

    <kbd> style

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-  Array.from(document.querySelectorAll('kbd')).forEach(function(el) {
+  Array.from(document.querySelectorAll('[data-mousetrap]')).forEach(function(el) {
     Mousetrap.bind(el.innerText.toLowerCase(), function() {
       window.location.href = el.parentElement.href
     })
