36a97f9bd8c88c139a52fae7bf063a9b85dd5b97
Gil Desmarais <git@desmarais.de>
2020-04-05 14:11:45 +0200

    refactor mousetrap

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,15 @@
-window.addEventListener('DOMContentLoaded', function() {
-  Array.from(document.querySelectorAll('[data-mousetrap]')).forEach(function(el) {
+;(function(window, document, Mousetrap) {
+  "use strict"
+
+  function bindMousetrap(el) {
     Mousetrap.bind(el.innerText.toLowerCase(), function() {
       window.location.href = el.parentElement.href
     })
+  }
+
+  window.addEventListener("DOMContentLoaded", function() {
+    Array.from(document.querySelectorAll("[data-mousetrap]")).forEach(
+      bindMousetrap
+    )
   })
-})
+})(window, document, Mousetrap)
