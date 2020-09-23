6136f47d59d1d25d8d81249ba8d50c0348037c39
Gil Desmarais <git@desmarais.de>
2020-09-23 11:44:02 +0200

    several things (yeah, bad commit message :D)

--- Gemfile
+++ Gemfile
@@ -2,10 +2,6 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'jekyll', '~> 4.1'
-# If you want to use GitHub Pages, remove the "gem "jekyll"" above and
-# uncomment the line below. To upgrade, run `bundle update github-pages`.
-# gem "github-pages", group: :jekyll_plugins
-
 gem 'sassc'
 
 # If you have any plugins, put them here!
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -41,10 +41,16 @@
   window.addEventListener("DOMContentLoaded", onDomContentLoad, options)
 
   if (window.console && window.console.log) {
-    const font = 'font-family: monospace; font-size:16px; line-height: 2';
+    const font = "font-family: monospace; font-size:16px; line-height: 2"
 
-    window.console.log('%c👋🏽 Hey there!', 'font-weight:bold;' + font);
-    window.console.log('%c🧐 Looks like you are interested in my work. That\'s cool.', font);
-    window.console.log('%c📧 If you like what you see, don\'t hesitate to contact me!', font);
+    window.console.log("%c👋🏽 Hey there!", "font-weight:bold;" + font)
+    window.console.log(
+      "%c🧐 Looks like you are interested in my work. That's cool.",
+      font
+    )
+    window.console.log(
+      "%c📧 If you like what you see, don't hesitate to contact me!",
+      font
+    )
   }
 })(window, document, Mousetrap)
