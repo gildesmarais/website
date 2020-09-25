298ee0070d62d48b7a547b89bbddf941f6d3ecb1
Gil Desmarais <git@desmarais.de>
2020-09-25 17:15:09 +0200

    feat(assets): add cache busting

--- _plugins/helpers.rb
+++ _plugins/helpers.rb
@@ -2,8 +2,15 @@
 
 require 'liquid'
 require 'digest'
+require 'digest/sha1'
 
 module Helpers
+  class CacheBuster < Liquid::Tag
+    def render(context)
+      context["site"]["time"].to_i.to_s(33)
+    end
+  end
+
   def sha256(string)
     Digest::SHA2.hexdigest string
   end
@@ -14,3 +21,4 @@ module Helpers
 end
 
 Liquid::Template.register_filter(Helpers)
+Liquid::Template.register_tag('cachebuster', Helpers::CacheBuster)
