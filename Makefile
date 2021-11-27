4a74744cacd260e3f5e3ec9c948dd675b9c324e1
Gil Desmarais <git@desmarais.de>
2021-11-27 19:23:03 +0100

    add to build process

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 default: clean fix
 
 fix:
-	npx prettier --write --no-semi --print-width 110 "**/*.html" "**/*.yml" "**/*.md" "(assets|_includes)/**/*.scss" "assets/javascript/main.js"
+	npx prettier --write --no-semi --print-width 110 "**/*.(yml|md)" "(assets|_includes)/**/*.scss" "assets/javascript/*/**.ts"
 	bundle exec rubocop -A
 
 clean:
@@ -10,6 +10,8 @@ clean:
 
 serve:
 	bundle check || bundle
+	yarn check || yarn
+	yarn build
 	open http://127.0.0.1:4000/blog/
 	bundle exec jekyll s --drafts --trace
 
@@ -24,7 +26,7 @@ ci-install:
 	bundle install --jobs 4 --retry 3
 	yarn check || yarn install
 
-build:
+build: | assets
 	echo "Fleamarket: pulling from discogs"
 	bin/discogs
 	echo "Blogroll: pulling from Newsblur"
@@ -37,4 +39,3 @@ build:
 post-deploy:
 	curl -fsS http://www.google.com/webmasters/sitemaps/ping?sitemap=https://gil.desmarais.de/sitemap.xml
 	curl -fsS http://www.bing.com/webmaster/ping.aspx?siteMap=https://gil.desmarais.de/sitemap.xml
-
--- assets/javascript/main.ts
+++ assets/javascript/main.ts
@@ -1,45 +1,43 @@
-
-import Headroom from "headroom.js";
-import Glide from '@glidejs/glide';
+import Headroom from "headroom.js"
+import Glide from "@glidejs/glide"
 
 namespace Utils {
   export function makeEmailLinkClickable(): void {
-    const body = encodeURIComponent(`Hi Gil,\nI found your contact details on ${window.location.href}\n`);
+    const body = encodeURIComponent(`Hi Gil,\nI found your contact details on ${window.location.href}\n`)
 
-    document.querySelectorAll("a[data-mail]").forEach(el => {
+    document.querySelectorAll("a[data-mail]").forEach((el) => {
       if (el instanceof HTMLAnchorElement) {
-        el.href = `mailto:${el.dataset.mail}?body=${body}`;
+        el.href = `mailto:${el.dataset.mail}?body=${body}`
       }
-    });
+    })
   }
 
   export function lockBodySizeOnNavMenuOpen(): void {
-    let el: HTMLInputElement = document.querySelector("#_nav-checkbox");
+    let el: HTMLInputElement = document.querySelector("#_nav-checkbox")
     el.addEventListener("change", ({ target }) => {
-      document.body.classList.toggle("body--nav-open", (target as HTMLInputElement).checked);
-    });
+      document.body.classList.toggle("body--nav-open", (target as HTMLInputElement).checked)
+    })
   }
 
   export function setupHeadroom(): void {
-    const headroom = new Headroom(document.querySelector("body > header"));
-    headroom.init();
+    const headroom = new Headroom(document.querySelector("body > header"))
+    headroom.init()
   }
 
   export function setupNavigationA11y(): void {
-    let checkbox: HTMLInputElement = document.querySelector("#_nav-checkbox");
+    let checkbox: HTMLInputElement = document.querySelector("#_nav-checkbox")
 
     const handler = (event: MouseEvent | KeyboardEvent) => {
-      if (event instanceof KeyboardEvent && event.code !== " ")
-        return;
+      if (event instanceof KeyboardEvent && event.code !== " ") return
 
-      event.preventDefault();
-      checkbox.checked = !checkbox.checked;
+      event.preventDefault()
+      checkbox.checked = !checkbox.checked
     }
 
-    document.querySelectorAll("[data-nav-toggle]").forEach(el => {
-      el.addEventListener("click", handler);
-      el.addEventListener("keydown", handler);
-    });
+    document.querySelectorAll("[data-nav-toggle]").forEach((el) => {
+      el.addEventListener("click", handler)
+      el.addEventListener("keydown", handler)
+    })
   }
 
   export function setupGlide(): void {
@@ -48,28 +46,27 @@ namespace Utils {
         el.id = `glide-${key}`
       }
 
-      const glide = new Glide(el.id);
-      glide.mount();
-    });
+      const glide = new Glide(el.id)
+      glide.mount()
+    })
   }
 
   export function printMessageOnConsole(): void {
     if (window.console && window.console.log) {
-      const font = "font-family: var(--font-mono); font-size: 1rem; line-height: 1.61";
+      const font = "font-family: var(--font-mono); font-size: 1rem; line-height: 1.61"
 
-      window.console.log("%c👋🏽 Hey there!", `font-weight: 700;${font}`);
-      window.console.log("%c🧐 Looks like you are interested in my work. That's cool.", font);
-      window.console.log("%c📧 If you like what you see, don't hesitate to contact me!", font);
+      window.console.log("%c👋🏽 Hey there!", `font-weight: 700;${font}`)
+      window.console.log("%c🧐 Looks like you are interested in my work. That's cool.", font)
+      window.console.log("%c📧 If you like what you see, don't hesitate to contact me!", font)
     }
   }
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-  Utils.setupHeadroom();
-  Utils.setupNavigationA11y();
-  Utils.lockBodySizeOnNavMenuOpen();
-  Utils.makeEmailLinkClickable();
-  Utils.setupGlide();
-  Utils.printMessageOnConsole();
-});
-
+  Utils.setupHeadroom()
+  Utils.setupNavigationA11y()
+  Utils.lockBodySizeOnNavMenuOpen()
+  Utils.makeEmailLinkClickable()
+  Utils.setupGlide()
+  Utils.printMessageOnConsole()
+})
