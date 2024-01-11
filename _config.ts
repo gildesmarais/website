cf4bb43a725355d6f7732e6bb2c5a983447c2f19
Gil Desmarais <git@desmarais.de>
2024-01-11 15:59:22 +1100

    style setup and index wip

---
+++ _config.ts
@@ -0,0 +1,42 @@
+import lume from "lume/mod.ts";
+import code_highlight from "lume/plugins/code_highlight.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import favicon from "lume/plugins/favicon.ts";
+import feed from "lume/plugins/feed.ts";
+import inline from "lume/plugins/inline.ts";
+import jsx from "lume/plugins/jsx.ts";
+import liquid from "lume/plugins/liquid.ts";
+import metas from "lume/plugins/metas.ts";
+// import minify_html from "lume/plugins/minify_html.ts";
+import picture from "lume/plugins/picture.ts";
+import remark from "lume/plugins/remark.ts";
+import sheets from "lume/plugins/sheets.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import transform_images from "lume/plugins/transform_images.ts";
+
+const site = lume(
+  {
+    location: new URL("https://gil.desmarais.de/"),
+  }
+);
+
+site.data("site", site);
+
+site.use(code_highlight());
+site.use(favicon());
+site.use(feed());
+site.use(inline());
+site.use(liquid());
+site.use(jsx());
+site.use(metas());
+// site.use(minify_html());
+site.use(picture());
+site.use(transform_images());
+site.use(remark());
+site.use(sheets());
+site.use(sitemap());
+site.use(esbuild(/* Options */));
+
+site.copy("assets");
+
+export default site;
