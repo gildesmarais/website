ffe9179b9a695e1824a1582ed100dd8b73600f02
Gil Desmarais <git@desmarais.de>
2025-09-29 22:42:46 +0300

    .

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -12,23 +12,11 @@ export const socials = [
     category: "professional",
   },
   {
-    name: "Bluesky",
-    url: "https://bsky.app/profile/gil.desmarais.de",
-    rel: "me",
-    category: "personal",
-  },
-  {
     name: "Soundcloud",
     url: "https://soundcloud.com/admiralgiesse",
     rel: "me",
     category: "personal",
-  },
-  {
-    name: "Reddit",
-    url: "https://www.reddit.com/user/der_gil",
-    rel: "me",
-    category: "personal",
-  },
+  }
 ].sort((a, b) => a.name.localeCompare(b.name))
 
 // Helper functions for filtering
