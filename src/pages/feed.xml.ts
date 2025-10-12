92d36804912e552a231602f35f9933e691496ce5
Gil Desmarais <git@desmarais.de>
2025-10-12 21:36:19 +0300

    lintfix

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,10 +2,7 @@ import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 
 export async function GET(context) {
-  const posts = await getCollection(
-    "blog",
-    ({ data }) => !data.draft && data.listed !== false,
-  )
+  const posts = await getCollection("blog", ({ data }) => !data.draft && data.listed !== false)
   return rss({
     title: "Gil Desmarais's Blog",
     description: "The latest posts from Gil Desmarais's blog.",
