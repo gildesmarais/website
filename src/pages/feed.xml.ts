cbd24ce2e025bf25627e415faed3d459fe8419b9
Gil Desmarais <gildesmarais@users.noreply.github.com>
2025-10-12 12:52:56 +0300

    Respect unlisted blog posts

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -2,7 +2,10 @@ import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 
 export async function GET(context) {
-  const posts = await getCollection("blog", ({ data }) => !data.draft && data.showcase)
+  const posts = await getCollection(
+    "blog",
+    ({ data }) => !data.draft && data.showcase && data.listed !== false,
+  )
   return rss({
     title: "Gil Desmarais's Blog",
     description: "The latest posts from Gil Desmarais's blog.",
