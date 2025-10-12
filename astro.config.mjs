326aa2c228b4fc271eeb335d3269e74f2c6d2133
Gil Desmarais <gildesmarais@users.noreply.github.com>
2025-10-12 12:28:52 +0300

    Move Vercel redirects into Astro config (#260)

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,61 @@ export default defineConfig({
   prefetch: true,
   redirects: {
     "/blog/watching-movies-alphabetically-round-2": "/blog/watching-movies-alphabetically",
+    "/blog/watching-movies-alphabetically-round-3": "/blog/watching-movies-alphabetically",
     "/blog/watching-movies-alphabetically-round-4": "/blog/watching-movies-alphabetically",
-    "/blog/picking-and-watching-movies-alphabetically": "/blog/watching-movies-alphabetically"
+    "/blog/picking-and-watching-movies-alphabetically": "/blog/watching-movies-alphabetically",
+    "/posts/:slug": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/posts/:slug/": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/posts/:slug/index.html": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug/": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug.html": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug/index.html": {
+      status: 301,
+      destination: "/blog/:slug",
+    },
+    "/feed.json": {
+      status: 301,
+      destination: "/feed.xml",
+    },
+    "/blogroll/:path*": {
+      status: 301,
+      destination: "/",
+    },
+    "/fleamarket/:path*": {
+      status: 301,
+      destination: "/",
+    },
+    "/sets/:path*": {
+      status: 301,
+      destination: "/",
+    },
+    "/tag/:path*": {
+      status: 301,
+      destination: "/blog",
+    },
+    "/index.html": {
+      status: 301,
+      destination: "/",
+    },
   },
   markdown: {
     rehypePlugins: [
